144aede3410672cb87d80b519114ad71c2fa3eda
Botinho12 <botivinicios77@gmail.com>
2025-05-19 21:11:21 -0300

    First commit - Criando o projeto

---
+++ CineFilms/src/app/app.component.spec.ts
@@ -0,0 +1,29 @@
+import { TestBed } from '@angular/core/testing';
+import { AppComponent } from './app.component';
+
+describe('AppComponent', () => {
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [AppComponent],
+    }).compileComponents();
+  });
+
+  it('should create the app', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    expect(app).toBeTruthy();
+  });
+
+  it(`should have the 'rdflix' title`, () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    expect(app.title).toEqual('rdflix');
+  });
+
+  it('should render title', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    const compiled = fixture.nativeElement as HTMLElement;
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, rdflix');
+  });
+});
---
+++ README.md
@@ -0,0 +1,40 @@
+# Movieflix
+> Projeto SPA Angular, para consumo de API 
+
+<br />
+<h1>☕ Executando o Movieflix</h1>
+
+Siga as instruções abaixo para executar o projeto em seu ambiente local:
+
+**Clone o repositório:**
+
+   ```
+   git clone https://github.com/HenryFelipe05/movieflix-angular.git
+   ```
+
+<h2>Passos Para Executar o Projeto Angular</h2>
+
+1. **Navegue até o diretório movieflix:**
+
+   ```
+   cd movieflix-angular
+   ```
+   ```
+   cd movieflix
+   ```
+
+2. **Instale o npm:**
+
+   ```
+   npm i
+   ```
+
+3. **Execute o servidor:**
+
+   ```
+   ng serve -o
+   ```
+
+**Agora você poderá começar a utilizar a aplicação!!**
+
+
